66dcdab5b54a7b2ac8058eca92822f25115e67a4
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-01-26 11:53:46 -0500

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -30,7 +30,7 @@ echo "***Building kernel...***"
 start_time=$(date +%Y.%m.%d-%I_%M)
 start_time_sum=$(date +%s)
 
-make ARCH=arm64 O=out CC="ccache clang" merge_kirin970_defconfig menuconfig
+make ARCH=arm64 O=out CC="ccache clang" kirin970_defconfig menuconfig
 make ARCH=arm64 O=out CC="ccache clang" -j$(nproc --all)
 
 end_time_sum=$(date +%s)
